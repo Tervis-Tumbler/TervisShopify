6e30f15af4a32d2833a5397c2576161b12c9a535
Hamilton Perez <hambone124@users.noreply.github.com>
2019-03-18 13:12:32 -0400

    Add Passwordstate GUID to Set-TervisShopifyCredential

--- TervisShopify.psm1
+++ TervisShopify.psm1
@@ -1,7 +1,7 @@
 #Requires -Modules ShopifyPowerShell,TervisPowershellJobs,TervisPasswordstatePowerShell
 
 function Set-TervisShopifyCredential {
-    $Credential = Get-PasswordstatePassword -ID 5729 | Select-Object UserName, Password
+    $Credential = Get-TervisPasswordstatePassword -Guid "4acc9b2a-080f-4f58-8cbd-843bcbc6d4ab" -AsCredential
     Set-ShopifyCredential -Credential $Credential
 }
 function Set-TervisShopifyEnvironment {
