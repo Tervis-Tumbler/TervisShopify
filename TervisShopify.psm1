d5d4d18ba75aa92240e821d84a91db3f6f26c60c
Hamilton Perez <hambone124@users.noreply.github.com>
2019-03-20 14:56:35 -0400

    Refactor New-ShopifyRestProduct to be neutral

--- TervisShopify.psm1
+++ TervisShopify.psm1
@@ -1,9 +1,37 @@
 #Requires -Modules ShopifyPowerShell,TervisPowershellJobs,TervisPasswordstatePowerShell
 
+Set-TervisShopifyEnvironment
+
 function Set-TervisShopifyCredential {
     $Credential = Get-TervisPasswordstatePassword -Guid "4acc9b2a-080f-4f58-8cbd-843bcbc6d4ab" -AsCredential
     Set-ShopifyCredential -Credential $Credential
 }
 function Set-TervisShopifyEnvironment {
     Set-TervisShopifyCredential
+}
+
+function New-TervisShopifyProduct {
+    param (
+        [Parameter(Mandatory)]$Title,
+        [Parameter(Mandatory)]$Description,
+        [Parameter(Mandatory)]$EBSItemNumber,
+        [Parameter(Mandatory)]$UPC,
+        [Parameter(Mandatory)]$Price,
+        $InventoryQuantity = 0,
+        $PublishedScope = "global",
+        $ShopName = "ospreystoredev"
+    )
+
+    $Params = @{
+        ShopName = $ShopName
+        Title = $Title
+        Body_HTML = $Description
+        SKU = $EBSItemNumber
+        Barcode = $UPC
+        Price = $Price
+        Inventory_Quantity = $InventoryQuantity
+        Published_Scope = $PublishedScope
+    }
+
+    New-ShopifyRestProduct @Params
 }
